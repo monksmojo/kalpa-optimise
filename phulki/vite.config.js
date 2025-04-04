ef3b6b430f24f2ed2ab2fec4a537ddfaf8e96c22
Bimochan Shrestha <bmochan@gmail.com>
2025-04-04 14:46:31 +0545

    frontend-setup: Tailwind and tsconfigs setup

--- phulki/vite.config.js
+++ phulki/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
+});
