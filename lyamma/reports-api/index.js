b70ec6a20d1571cbfe2e31ea91660f0ea76a3633
Bimochan Shrestha <bmochan@gmail.com>
2025-04-05 11:15:28 +0545

    final-report: Function to send dynamodb response

--- lyamma/reports-api/index.js
+++ lyamma/reports-api/index.js
@@ -1,32 +1,36 @@
 import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
 
 const client = new DynamoDBClient({ region: "us-east-1" });
 
 export const handler = async (event) => {
   const tableName = "final_report";
-  const account_id = event.accountId; // Make sure to pass this in the event
-
+  const account_id = event.queryStringParameters.accountId; // Make sure to pass this in the event
+  if (!account_id) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Missing accountId in event" })
+    };
+  }
   const params = {
     TableName: tableName,
-    KeyConditionExpression: "account_id = :account_id",
+    KeyConditionExpression: "#account_id = :account_id",
+    ExpressionAttributeNames: {
+      "#account_id": "account_id"
+    },
     ExpressionAttributeValues: {
-      ":account_id": account_id
+      ":account_id": { S: account_id }
     }
   };
 
   try {
     const command = new QueryCommand(params);
     const response = await client.send(command);
-
-    // Map the response items to plain JavaScript objects
-    const items = response.Items?.map((item) => ({
-      accountId: item.account_id // Access the attribute directly
-      // Add other attributes as needed
-    }));
+    const items = response.Items?.map((item) => unmarshall(item));
 
     return {
       statusCode: 200,
-      body: JSON.stringify(items) // Return the items as JSON
+      body: items // Return the items as JSON
     };
   } catch (err) {
     console.error("DynamoDB query failed:", err);
